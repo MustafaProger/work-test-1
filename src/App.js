76d2bb11d8227114a946eca0757c19e6cc859172
DIOR <diertojiev@yandex.ru>
2024-11-21 18:04:50 +0300

    Section "Selector" is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Selector from "./components/selector/selector";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		selectorState: {
+			selectedOption: "Выберите опцию",
+			options: ["Сертификат на 50000 руб", "Сертификат на 20000 руб", "Сертификат на 10000 руб", "Сертификат на 5000 руб"],
+			isOpen: false,
+		},
+	};
+
+	toggleOptions = () => {
+		this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+	};
+
+	handleOptionClick = (option) => {
+		this.setState({ selectedOption: option, isOpen: false });
+	};
+
+	updateSelectorState = (newState) => {
+		this.setState((prevState) => ({
+			selectorState: {
+				...prevState.selectorState,
+				...newState,
+			},
+		}));
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<div className='container'>
+					<Selector
+						toggleOptions={this.toggleOptions}
+						handleOptionClick={this.handleOptionClick}
+						selectorState={this.state.selectorState}
+						updateSelectorState={this.updateSelectorState}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
