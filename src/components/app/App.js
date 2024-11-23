ab05c196b88e224d88415db793ab85a38ff25cfb
DIOR <diertojiev@yandex.ru>
2024-11-23 13:38:22 +0300

    Function with API and Loading
    1. Создал глобальное API с данными об сертификатах.
    2. Создал функцию, которая получает эти данные, затем делает верстку.
    3. Пока данные получаются, происходит загрузка минимум в 1 сек.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,153 +5,108 @@ import Navbar from "../navbar/Navbar";
 
 import "./App.css";
 
-import vkusvill from "../../assets/img/vkusvill.jpg";
-import mvideo from "../../assets/img/mvideo.jpg";
-import wink from "../../assets/img/wink.jpg";
-import khasl from "../../assets/img/khasl.png";
-import aldarado from "../../assets/img/aldarado.jpg";
-import chitaiGorod from "../../assets/img/chitai-gorod.png";
-import litres from '../../assets/img/litres.jpg'
-import perecrectok from '../../assets/img/perecrectok.jpg'
-import tefal from '../../assets/img/tefal.png'
-
-
+// import vkusvill from "../../assets/img/vkusvill.jpg";
+// import mvideo from "../../assets/img/mvideo.jpg";
+// import wink from "../../assets/img/wink.jpg";
+// import khasl from "../../assets/img/khasl.png";
+// import aldarado from "../../assets/img/aldarado.jpg";
+// import chitaiGorod from "../../assets/img/chitai-gorod.png";
+// import litres from '../../assets/img/litres.jpg'
+// import perecrectok from '../../assets/img/perecrectok.jpg'
+// import tefal from '../../assets/img/tefal.png'
 
 class App extends Component {
-  state = {
-    products: [
-      {
-        id: 1,
-        name: "ВкусВилл",
-        price: 1000,
-        description:
-          "ВкусВилл – интернет-магазин и розничная сеть супермаркетов продуктов для здорового питания.",
-        image: vkusvill,
-      },
-      {
-        id: 2,
-        name: "М.Видео",
-        price: 10000,
-        description:
-          "М.видео — лидер по продаже электроники и бытовой техники среди розничных сетей России. По всем вопросам вы сможете обратиться в нашу службу поддержи на почту support@giftery.ru и по телефону 8-800-700-90-90.",
-        image: mvideo,
-      },
-      {
-        id: 3,
-        name: "Wink",
-        price: 500,
-        description:
-          "Wink – умный развлекательный видеосервис. Тысячи фильмов, сериалов и сотни ТВ-каналов на любой вкус в высоком качестве. Тариф «Wink» включает в себя: от 120 ТВ-каналов на любой вкус 27 000 фильмов, сериалов и мультфильмов.",
-        image: wink,
-      },
-      {
-        id: 4,
-        name: "Хасл",
-        price: 5000,
-        description:
-          "Хасл – твой надежный партнер в мире спортивной моды.В нашем широком ассортименте: кроссовки, ботинки, одежда ведущих брендов Nike, Adidas, Reebok и других.Бесплатная доставка и оплата при получении.Выгодные скидки:регулярные акции и распродажи!",
-        image: khasl,
-      },
-      {
-        id: 5,
-        name: "Эльдорадо",
-        price: 15000,
-        description:
-          "Эльдорадо — лидер по продаже электроники и бытовой техники среди розничных сетей России. По всем вопросам вы сможете обратиться в нашу службу поддержи на почту support@giftery.ru и по телефону 8-800-700-90-90.",
-        image: aldarado,
-      },
-      {
-        id: 6,
-        name: "Читай город",
-        price: 500,
-        description:
-          "«Читай-город» – российская федеральная розничная сеть книжных магазинов и крупный интернет-магазин книг. В нём вы можете заказывать книги в любое время 24 часа в сутки. Если хотите угадать с подарком, выберите наш электронный сертификат!",
-        image: chitaiGorod,
-      },
-      {
-        id: 7,
-        name: "Литрес",
-        price: 300,
-        description:
-          "На выбранную вами сумму счастливый обладатель сертификата сможет пополнить свой счет Литрес и распоряжаться им как угодно: покупать книги или оплатить Абонемент.",
-        image: litres,
-      },
-      {
-        id: 8,
-        name: "Перекресток",
-        price: 2500,
-        description:
-          "«Перекресток» превращает привычный поход в магазин в увлекательное гастрономическое путешествие. Сторонник здорового питания или ценитель изысканных блюд? Здесь каждый найдет что-то себе по душе!",
-        image: perecrectok,
-      },
-      {
-        id: 9,
-        name: "Tefal",
-        price: 5000,
-        description:
-          "Официальный интернет-магазин бытовой техники и посуды Tefal в России.Также, на сайте представлены товары брендов Moulinex, Rowenta, Krups, Emsa.",
-        image: tefal,
-      },
-    ],
-    selectedProduct: null,
-    isModalOpen: false,
-    searchValue: "",
-  };
+	state = {
+		products: [],
+		loading: true,
+		selectedProduct: null,
+		isModalOpen: false,
+		searchValue: "",
+	};
 
-  openModal = (product) => {
-    this.setState({ selectedProduct: product, isModalOpen: true });
-  };
+	openModal = (product) => {
+		this.setState({ selectedProduct: product, isModalOpen: true });
+	};
 
-  closeModal = () => {
-    this.setState({ selectedProduct: null, isModalOpen: false });
-  };
+	closeModal = () => {
+		this.setState({ selectedProduct: null, isModalOpen: false });
+	};
 
-  updateStateSearchValue = (newSearchValue) => {
-    this.setState({ searchValue: newSearchValue });
-  };
+	updateStateSearchValue = (newSearchValue) => {
+		this.setState({ searchValue: newSearchValue });
+	};
 
-  productAfterSearch = (items, search) => {
-    if (search.length === 0) return items;
+	productAfterSearch = (items, search) => {
+		if (search.length === 0) return items;
 
-    return items.filter((item) => {
-      return item.name.toLowerCase().includes(search.toLowerCase());
-    });
-  };
+		return items.filter((item) => {
+			return item.name.toLowerCase().includes(search.toLowerCase());
+		});
+	};
 
-  render() {
-    const { products, selectedProduct, isModalOpen, searchValue } = this.state;
+	delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-    const filteredProducts = this.productAfterSearch(products, searchValue);
+	async componentDidMount() {
+		// Таймер, чтобы подождать хотя бы 2 секунды перед загрузкой
+		await this.delay(1000);  // Задержка 2 секунды (2000 миллисекунд)
 
-    return (
-      <div className="App">
-        <div className="container">
-          <Navbar updateStateSearchValue={this.updateStateSearchValue} />
-          <h1
-            style={{ fontSize: 40, textAlign: "center", marginBottom: "50px" }}
-          >
-            Подарочные сертификаты
-          </h1>
-          <div className="cards">
-            {filteredProducts && filteredProducts.length > 0 ? (
-              filteredProducts.map((product) => (
-                <Card
-                  key={product.id}
-                  product={product}
-                  onCardClick={this.openModal}
-                />
-              ))
-            ) : (
-              <h1 style={{border: "3px solid #02b5e1", padding: 20}}>Нет сертификатов с название {this.state.searchValue}</h1>
-            )}
-          </div>
-        </div>
-        {isModalOpen && (
-          <Modal product={selectedProduct} onClose={this.closeModal} />
-        )}
-      </div>
-    );
-  }
+		try {
+		  const response = await fetch("https://destiny-handy-cover.glitch.me/products");
+		  const data = await response.json();
+		  this.setState({ products: data, loading: false });  // Сохраняем данные в state
+		} catch (error) {
+		  console.error("Error:", error);
+		  this.setState({ loading: false });  // При ошибке останавливаем индикатор загрузки
+		}
+	  }
+
+	render() {
+		const { products, selectedProduct, isModalOpen, searchValue, loading } =
+			this.state;
+
+		const filteredProducts = this.productAfterSearch(products, searchValue);
+
+		if (loading) {
+			return (
+				<div class='loader-wrapper'>
+					<div class='custom-loader'></div>
+				</div>
+			);
+		}
+
+		return (
+			<div className='App'>
+				<div className='container'>
+					<Navbar updateStateSearchValue={this.updateStateSearchValue} />
+					<h1
+						style={{ fontSize: 40, textAlign: "center", marginBottom: "50px" }}>
+						Подарочные сертификаты
+					</h1>
+					<div className='cards'>
+						{filteredProducts && filteredProducts.length > 0 ? (
+							filteredProducts.map((product) => (
+								<Card
+									key={product.id}
+									product={product}
+									onCardClick={this.openModal}
+								/>
+							))
+						) : (
+							<h1 style={{ border: "3px solid #02b5e1", padding: 20 }}>
+								Нет сертификатов с названием {this.state.searchValue}
+							</h1>
+						)}
+					</div>
+				</div>
+				{isModalOpen && (
+					<Modal
+						product={selectedProduct}
+						onClose={this.closeModal}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
