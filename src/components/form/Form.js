6ce284f05006ca79aae7f8b73ee855b6216ea7ea
DIOR <diertojiev@yandex.ru>
2024-11-24 22:25:57 +0300

    Validation for form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,35 +1,66 @@
 import React, { Component } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-
-import App from "../app/App";
+import { Link } from "react-router-dom";
+import validation from "./validation";
+import phoneInput from "./phoneInput";
 import "./Form.css";
 
 class Form extends Component {
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		validation(document.querySelector(".form"), "#name", "#phone", "#email");
+	};
+
+	handleFloatingLabel() {
+		const labels = document.querySelectorAll(".form__label");
+		const inputs = document.querySelectorAll(".form__group input");
+
+		inputs.forEach((input, index) => {
+			input.addEventListener("input", () => {
+				const label = labels[index]; // Связываем конкретный инпут с его лейблом
+				if (input.value !== "") {
+					label.style.cssText =
+						"font-size: 14px; transform: translateY(-125%);";
+				} else {
+					label.style.cssText = ""; // Возвращаем лейбл в исходное положение, если инпут пустой
+				}
+			});
+		});
+	}
+
+	// Вызов метода после первого рендера компонента
+	componentDidMount() {
+		this.handleFloatingLabel(); // Вызываем функцию плавающих лейблов
+		phoneInput();
+	}
+
 	render() {
 		const { product, price } = this.props;
 
 		return (
 			<form
 				className='form'
-				action='#'>
+				onSubmit={this.handleSubmit} // Обработчик отправки формы
+			>
 				<div className='form__container'>
 					<h3
 						style={{
 							textAlign: "center",
 							marginBottom: 40,
-						}}>{`Сертификат от "${product}" на ${price} руб`}</h3>
+						}}>
+						{`Сертификат от "${product}" на ${price} руб`}
+					</h3>
 					<div className='form__group'>
 						<input
 							type='text'
 							className='form__input'
-							placeholder='Имя'
 							id='name'
-							required
 						/>
 						<label
 							htmlFor='name'
 							className='form__label'>
-							Имя<span style={{color: 'orange', fontSize: 20}}>*</span>
+							Имя
+							<span style={{ color: "orange", fontSize: 20 }}>*</span>
 						</label>
 					</div>
 
@@ -37,46 +68,43 @@ class Form extends Component {
 						<input
 							type='tel'
 							className='form__input'
-							placeholder='Номер телефона'
 							id='phone'
-							required
+							data-tel-input
 						/>
 						<label
 							htmlFor='phone'
 							className='form__label'>
-							Номер телефона<span style={{color: 'orange', fontSize: 20}}>*</span>
+							Номер телефона
+							<span style={{ color: "orange", fontSize: 20 }}>*</span>
 						</label>
 					</div>
 					<div className='form__group'>
 						<input
 							type='email'
 							className='form__input'
-							placeholder='Email'
 							id='email'
-							required
 						/>
 						<label
 							htmlFor='email'
 							className='form__label'>
-							Почты<span style={{color: 'orange', fontSize: 20}}>*</span>
+							Почта
+							<span style={{ color: "orange", fontSize: 20 }}>*</span>
 						</label>
 					</div>
-					<div class='form__btns'>
+
+					<div className='form__btns'>
 						<Link
 							to='/'
 							className='modal__button'>
 							Назад
 						</Link>
-						<Link
-							to='https://auth.robokassa.ru/'
+						<button
+							type='submit'
 							className='modal__button'>
 							Оформить
-						</Link>
+						</button>
 					</div>
 				</div>
-				{/* <Routes>
-          <Route path="/" element={<App/>}/>
-        </Routes> */}
 			</form>
 		);
 	}
---
+++ src/components/form/validation.js
@@ -0,0 +1,111 @@
+export default function validation(form, inputName, inputPhone, inputEmail) {
+    const nameInput = form.querySelector(inputName);
+    const phoneInput = form.querySelector(inputPhone);
+    const emailInput = form.querySelector(inputEmail);
+
+    let isValid = true;
+
+    let message = {
+        name: {
+            required: 'Введите имя пользователя',
+            minLength: 'Введите не менее 2 символов',
+            correct: 'Имя не должно содержать цифр'
+        },
+        phone: {
+            required: 'Введите номер телефона',
+            minLength: 'Введите не менее 11 символов',
+            correct: 'Номер не должен содержать буквы',
+        },
+        email: {
+            required: 'Введите электронную почту',
+            correct: 'Введите корректный email'
+        }
+    }
+
+    let { name, phone, email } = message;
+
+    errorWork(nameInput, 'name', 2, name);
+    errorWork(phoneInput, 'phone', 18, phone);
+
+    emailErrorWork();
+
+
+    return isValid;
+
+    function errorWork(input, classError, length, regex, message) {
+        createError(input, classError);
+        const error = form.querySelector(`.${classError}__error`);
+
+        const validateField = () => {
+            validationNamePhone(input, error, length, regex, message);
+        };
+
+        input.removeEventListener('input', validateField);
+        input.addEventListener('input', validateField);
+        validateField();
+    }
+
+    function emailErrorWork() {
+        createError(emailInput, 'email');
+        const emailError = form.querySelector('.email__error');
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        const validateEmailField = () => {
+            validationEmail(emailInput, emailError, emailRegex, email);
+        };
+
+        emailInput.removeEventListener('input', validateEmailField);
+        emailInput.addEventListener('input', validateEmailField);
+        validateEmailField();
+    }
+
+
+    function validationNamePhone(input, error, length, message) {
+        let fieldIsValid = true;
+
+        if (input.value.trim() === '') {
+            error.innerHTML = message.required;
+            error.style.display = 'block';
+            fieldIsValid = false;
+        } else if (input.value.length < length) {
+            error.innerHTML = message.minLength;
+            error.style.display = 'block';
+            fieldIsValid = false;
+        } else {
+            error.style.display = 'none';
+        }
+
+        if (!fieldIsValid) isValid = false;
+    }
+
+
+
+    function validationEmail(input, error, regex, message) {
+        let fieldIsValid = true;
+
+        if (input.value.trim() === '') {
+            error.innerHTML = message.required;
+            error.style.display = 'block';
+            fieldIsValid = false;
+        } else if (!regex.test(input.value)) {
+            error.innerHTML = message.correct;
+            error.style.display = 'block';
+            fieldIsValid = false;
+        } else {
+            error.style.display = 'none';
+        }
+
+        if (!fieldIsValid) isValid = false;
+    }
+
+
+    function createError(input, name) {
+        let error = form.querySelector(`.${name}__error`);
+        if (!error) {
+            error = document.createElement('span');
+            error.classList.add('error', `${name}__error`);
+            error.style.display = 'none';
+            input.insertAdjacentElement('afterend', error);
+        }
+    }
+}
