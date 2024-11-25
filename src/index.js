c1b1fc897da287f9716c9a8c81dd41dd2f39d016
DIOR <diertojiev@yandex.ru>
2024-11-25 12:18:59 +0300

    Fix bag with URL

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="work-test-1">
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
